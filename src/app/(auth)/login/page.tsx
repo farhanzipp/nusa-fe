dceca5b3eeebe5a819a180fad0a9c3db43b355d4
farhanzipp <farhanaji317@gmail.com>
2024-09-12 22:30:20 +0700

    feat: shadcn login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,8 +5,7 @@ export default function LoginPage() {
     
   return (
     <div className='w-full h-screen flex flex-col items-center justify-center'>
-        <h1>Login Page</h1>
-        <div className='w-1/4'>
+        <div className='w-5/6 md:w-[400px]'>
             <LoginForm />
         </div>
     </div>
--- src/components/forms/login-form.tsx
+++ src/components/forms/login-form.tsx
@@ -9,6 +9,10 @@ import { useForm } from 'react-hook-form';
 import Cookies from 'js-cookie';
 import { getDecodedToken } from '@/util/jwt-decode';
 import { redirectByRole } from '@/util/route-by-role';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Loader2 } from 'lucide-react';
 
 export default function LoginForm() {
     const {
@@ -40,31 +44,40 @@ export default function LoginForm() {
     };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-            {...register("email")}
-            type="email"
-            placeholder="email"
-            className='py-2'
-        />
-        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
-        <input
-            {...register("password" )}
-            type="password"
-            placeholder="password"
-            className='py-2'
-        />
-        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
-        {errors.root?.serverError && (
-          <p className="text-sm text-red-500">{errors.root.serverError.message}</p>
-        )}
-        <button
-            type="submit"
-            disabled={isSubmitting}
-            className='w-full bg-blue-500 text-white py-2 disabled:bg-gray-500 rounded'
-            >
-            {isSubmitting ? "Loading..." : "Login"}
-        </button>
-    </form>
+    <Card>
+        <CardHeader className='text-center'>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>Login to your account</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Input
+                    {...register("email")}
+                    type="email"
+                    placeholder="email"
+                />
+                {errors.email && <p className='text-xs text-red-500'>{errors.email.message}</p>}
+                <Input
+                    {...register("password" )}
+                    type="password"
+                    placeholder="password"
+                    className='mt-4'
+                />
+                {errors.password && <p className='text-xs text-red-500'>{errors.password.message}</p>}
+                {errors.root?.serverError && (
+                <p className="text-xs text-red-500">{errors.root.serverError.message}</p>
+                )}
+                <Button
+                    type="submit"
+                    disabled={isSubmitting} 
+                    className="w-full mt-7"
+                    >
+                    {isSubmitting ? (
+                       <Loader2 className='mr-2 h-4 w-4 animate-spin' /> 
+                    ) : "Login"}
+                </Button>
+            </form>
+    </CardContent>
+    </Card>
   )
 }
